a561a1869921b9a0147fb354b42536f2d11376cb
Amirov735 <asniduna319@gmail.com>
2026-04-07 08:42:44 +0300

    Update NEW_config_v3.py

--- script4/NEW_config_v3.py
+++ script4/NEW_config_v3.py
@@ -5,7 +5,13 @@ from concurrent.futures import ThreadPoolExecutor
 
 # Список твоих источников
 URLS = [
-    
+
+    "https://raw.githubusercontent.com/ewecrow78-gif/whitelist1/main/list.txt",
+    "https://raw.githubusercontent.com/ERRORQSFG/fuckwhitelists/refs/heads/main/sub.txt",
+    "https://raw.githubusercontent.com/ssavnayt/AWCFG-CONFIG-LIST/refs/heads/main/Configs-AUTO.txt",
+    "https://raw.githubusercontent.com/ssavnayt/AWCFG-CONFIG-LIST/refs/heads/main/Configs-all-country.txt",
+    "https://raw.githubusercontent.com/Maskkost93/kizyak-vpn-4.0/refs/heads/main/kizyakbeta6.txt",
+    "https://raw.githubusercontent.com/VSd223/vpn/refs/heads/main/vpn",
     
     # Сюда можно бахнуть еще 10-20 ссылок, он все переварит
 ]
@@ -56,7 +62,7 @@ def main():
     final_list = [c for c in unique_configs if len(c) > 20]
 
     # 3. Сохраняем в файл
-    with open("configs.txt", "w", encoding="utf-8") as f:
+    with open("NEW_config_v3.txt", "w", encoding="utf-8") as f:
         f.write("\n".join(final_list))
 
     print(f"\n[+] Сбор завершен!")
